74981ecd73f98f105addc6c2536b1845718198e4
Mart Raudsepp <mart.raudsepp@artecdesign.ee>
2009-07-23 17:08:55 +0300

    autotoolify (half-automatically)
    Also some anjuta stuff

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.c
+ * Copyright (C) Mart Raudsepp 2009 <mart.raudsepp@artecdesign.ee>
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <config.h>
+
+#include <glib.h>
+
+
+int
+main (int argc, char *argv[])
+{
+	return 0;
+}
