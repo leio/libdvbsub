3169139b4ee68f41548ca067d3c798f9a8b74882
Mart Raudsepp <mart.raudsepp@artecdesign.ee>
2009-09-04 19:31:50 +0300

    If packet is cut, ignore it, instead of letting subsequent code flow walk into memory we don't own.
    We need to debug this further later on and perhaps buffer and glue data together to form a full
    packet, but this should hopefully meanwhile fix any potential crashes and warning serial port printing
    storm.

--- src/dvb-sub.c
+++ src/dvb-sub.c
@@ -1693,6 +1693,10 @@ dvb_sub_feed (DvbSub *dvb_sub, guint8 *data, gint len)
 
 		/* FIXME: If the packet is cut, we could be feeding data more than we actually have here, which breaks everything. Probably need to buffer up and handle it,
 		 * FIXME: Or push back in front to the file descriptor buffer (but we are using read, not libc buffered fread, so that idea might not be possible )*/
+		if (pos > len) {
+			g_warning ("!!!!!!!!!!! claimed PES packet length was %d, but we only had %d bytes left... Cut packet, ignoring !!!!!!!!!", PES_packet_len, len);
+			return -4;
+		}
 		dvb_sub_feed_with_pts (dvb_sub, pts, data + pos, PES_packet_len - PES_packet_header_len - 3); /* 2 bytes between PES_packet_len and PES_packet_header_len fields, minus header_len itself */
 		pos += PES_packet_len - PES_packet_header_len - 3;
 		g_print("Finished PES packet number %u\n", counter);
